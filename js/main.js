2dc60028d1e6a6e7e3fc386fa1097952fad8d167
lilymenal <liliana.mendoza.alvear@gmail.com>
2021-01-28 11:00:47 +0100

    initial main js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,52 @@
 'use strict';
 
 const pruebaTitle = document.querySelector ('.title');
+let userRandomNumber = document.querySelector ('.jsintroduce')
+const userClickButton = document.querySelector ('.jsbutton')
+let trackForUser = document.querySelector ('.jstrack');
 
 console.log(`Verificar`);
+function handleTrack() {
+    let gameTrack = trackForUser
+
+
+    if (gameTrack === "") {
+        trackForUser.innerHTML = 'Pista:Escribe el numero y dale a Prueba';
+    } else if (gameTrack = userRandomNumber) {
+        trackForUser.innerHTML = 'Has ganado';
+    }
+    else if (gameTrack < userRandomNumber) {
+        trackForUser.innerHTML = 'Demasiado bajo';
+    }
+    else if (gameTrack > userRandomNumber) {
+        trackForUser.innerHTML = 'Demasiado alto';
+    }
+    else if (gameTrack > 100 ) {
+        trackForUser.innerHTML = 'Número inválido';
+    }
+}
+
+userClickButton.addEventListener('keyup', handleTrack);
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
-const randomNumber = Math.ceil(Math.random() * 100);
+const max = 100;
+const randomNumbermax = Math.ceil(Math.random() * max);
+console.log (`número aleatorio ${randomNumbermax}`);
+if (userRandomNumber === randomNumbermax) {
 
-console.log (`número aleatorio ${randomNumber}`);
+      userWin = 'Has ganado';
+    console.log (`${userWin}`);
+
+}
+else if (userRandomNumber > randomNumbermax) {
+
+}
+
+
+userRandomNumber = 3;
+
+userClickButton.addEventListener ( 'click' , getRandomNumber)
+
+
