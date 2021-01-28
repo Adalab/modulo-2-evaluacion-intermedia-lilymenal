8f0dd3b82d65e364957236ff4352ea875d0fc92e
lilymenal <liliana.mendoza.alvear@gmail.com>
2021-01-28 10:04:57 +0100

    randomnumber

--- js/main.js
+++ js/main.js
@@ -6,4 +6,7 @@ console.log(`Verificar`);
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-    }
+}
+const randomNumber = Math.ceil(Math.random() * 100);
+
+console.log (`número aleatorio ${randomNumber}`);
